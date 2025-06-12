c7d68f5cade6a3c2236fd02dc401f42ccb13be80
namuunaa <namuunaa@namuunaas-MacBook-Air.local>
2025-06-12 11:22:57 +0800

    BE add order-route and contoller(postcontrroller, getcontroller

--- bookme/src/app/order/_components/employeeCard.tsx
+++ bookme/src/app/order/_components/employeeCard.tsx
@@ -1,10 +1,29 @@
-const EmloyerCard = () => {
-    return (
-        <div className="w-fit border shadow aspect-1/1 border flex justify-center items-center ">
-            <div className=""></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    )
+"use client"
+type EmloyerCardProp = {
+    setIsSelectEmployee: (value: string) => void
 }
+import { useState } from "react";
+
+const EmployeeCard = ({ setIsSelectEmployee }: EmloyerCardProp) => {
+    const [isSelect, setIsSelect] = useState(false);
+    return (
+        <div
+            onClick={() => {
+                if (!isSelect) {
+                    setIsSelectEmployee("hooson bish");
+                } else {
+                    setIsSelectEmployee("");
+                }
+                setIsSelect(!isSelect);
+            }}
+            className={isSelect
+                ? "w-fit border border-blue-500 shadow rounded-xl relative aspect-1/1 border px-4 py-8 flex flex-col justify-center gap-6 items-center  hover:bg-zinc-100 "
+                : "w-fit border  border-gray-300 shadow rounded-xl relative aspect-1/1 border px-4 py-8 flex flex-col justify-center gap-6 items-center hover:bg-zinc-100  "}
+        >
+            <img className="w-14 h-14 rounded-full" />
+            <div className=" w-4 " >нэр</div>
+            <div className="">танилцуулга</div>
+        </div>
+    );
+}
+export default EmployeeCard
---
+++ bookme/src/app/order/_components/timingSelection.tsx
@@ -0,0 +1,42 @@
+
+import { PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Popover } from "@radix-ui/react-popover"
+import { CalendarIcon } from "lucide-react"
+import { useState } from "react"
+import { Calendar } from "@/components/ui/calendar"
+import UpdateEmployee from "./updateEmployeeDialog"
+
+type TimingProps = {
+    isSelectEmployee: string
+}
+function AddTime({ isSelectEmployee }: TimingProps) {
+    const [open, setOpen] = useState(false)
+    const [date, setDate] = useState<Date | undefined>(undefined)
+
+    return (
+        <div className="w-full pr-4">
+            <div className="flex w-full justify-between items-center ">
+                <UpdateEmployee isSelectEmployee={isSelectEmployee} />
+                <div className=" relative z-10">
+                    <Popover open={open} onOpenChange={setOpen}>
+                        <PopoverTrigger asChild>
+                            <CalendarIcon />
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto overflow-hidden p-2 z-10 bg-white " align="center">
+                            <Calendar className="flex gap-3"
+                                mode="single"
+                                selected={date}
+                                captionLayout="dropdown"
+                                buttonVariant="ghost"
+                                onSelect={(date) => {
+                                    setDate(date)
+                                    setOpen(false)
+                                }}
+                            />
+                        </PopoverContent>
+                    </Popover></div>
+            </div>
+        </div>
+    )
+}
+export default AddTime
