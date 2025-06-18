38bdd075fe4bef3f97e00c58a25294e554d46a3d
24LP0893 24LP0893 <24LP0893@administrators-iMac-2.local>
2025-06-18 17:07:20 +0800

    Employee Data edit success

--- bookme/src/app/company/[companyName]/settings/_components/EmployeeCard.tsx
+++ bookme/src/app/company/[companyName]/settings/_components/EmployeeCard.tsx
@@ -1,23 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 import { EmployeeType } from "../_providers/CompanySettingsProvider";
-
-export const EmployeeCard = (
-  {
-    employeeName,
-    description,
-    profileImage,
-  }: EmployeeType
-) => {
-  console.log(profileImage);
-
+export const EmployeeCard = ({
+  employeeName,
+  description,
+  profileImage,
+}: EmployeeType) => {
   return (
-    <div className="w-full h-full bg-[#f7f7f7] rounded-4xl flex flex-col justify-between items-center p-5 gap-3">
-      <img src={`${profileImage}`} className="rounded-2xl" />
+    <div className="w-full h-full rounded-4xl flex flex-col justify-between items-center p-5 gap-3">
+      <img src={`${profileImage}`} className="rounded-2xl h-[200px]" />
       <div className="w-full  rounded-2xl px-3 ">
-        <div className="bg-white w-fit rounded-2xl p-3 ">  Нэр: {employeeName}</div>
+        <div className="bg-white w-fit rounded-2xl p-3 ">
+          Нэр: {employeeName}
+        </div>
       </div>
       <div className="w-full  rounded-2xl px-3 ">
-        <div className="bg-white w-full rounded-2xl p-3 ">  Танилцуулга: {description}</div>
+        <div className="bg-white w-full rounded-2xl p-3 ">
+          Танилцуулга: {description}
+        </div>
       </div>
     </div>
   );
---
+++ bookme/src/app/company/[companyName]/settings/_components/EmployeeEditCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import { EmployeeType } from "../_providers/CompanySettingsProvider";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PenSvg } from "./assets/PenSvg";
+import { api } from "@/axios";
+import { toast } from "sonner";
+
+type Props = {
+  employee: EmployeeType;
+  employeeIndex: number;
+};
+export const EmployeeEditCard = ({ employee, employeeIndex }: Props) => {
+  const [edit, setEdit] = useState(true);
+  const [newData, setNewData] = useState({
+    employeeName: employee.employeeName,
+    description: employee.description,
+  });
+
+  const handleChangeEmployeeData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setNewData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSaveData = async () => {
+    console.log(newData);
+
+    try {
+      const request = await api.put(`/employee/${employee._id}`, {
+        employeeName: newData.employeeName,
+        description: newData.description,
+      });
+      toast.success("Ажилтны мэдээлэл амжилттай солигдлоо.");
+    } catch (error) {
+      console.error(error, "Ажилтны мэдээлэл хадгалахад алдаа гарлаа.");
+      toast.error("Ажилтны мэдээлэл хадгалахад алдаа гарлаа.");
+    }
+  };
+
+  return (
+    <div className="flex gap-5 items-center w-full h-fit border-[1px] rounded-2xl p-3">
+      <div className="flex flex-col items-center gap-2">
+        <div>№</div>
+        <div>{employeeIndex + 1}</div>
+      </div>
+      <div className="flex flex-col items-center  gap-2">
+        <div>Ажилтны нэр</div>
+        <div className="w-full ">
+          <Input
+            name="employeeName"
+            onChange={handleChangeEmployeeData}
+            disabled={edit}
+            value={newData.employeeName}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center  gap-2">
+        <div>Нэмэлт мэдээлэл</div>
+        <div className="w-[200px]  overflow-hidden">
+          <Input
+            name="description"
+            onChange={handleChangeEmployeeData}
+            disabled={edit}
+            value={newData.description}
+          />
+        </div>
+      </div>
+      <Button variant={"outline"} onClick={() => setEdit(!edit)}>
+        <PenSvg />
+      </Button>
+      {!edit && <Button onClick={handleSaveData}>Хадгалах</Button>}
+    </div>
+  );
+};
