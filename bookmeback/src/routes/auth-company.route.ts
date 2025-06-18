edb46bb2f4f355104091ce506ceaa7607cf71cb8
ulziinaranzo <ulziinaranzorig@gmail.com>
2025-06-18 09:23:26 +0800

    company controllers fixed

--- bookmeback/src/routes/auth-company.route.ts
+++ bookmeback/src/routes/auth-company.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { signupCompanyController } from "../controllers/(companyAuth)/auth/sign-up-company";
 import { signInCompanyController } from "../controllers/(companyAuth)/auth/sign-in-company";
+import { companyAuthMiddleware } from "../middlewares/companyAuthMiddleware";
+import { getCompanyAuthMe } from "../controllers/(companyAuth)/auth/company-get-me";
 
 const authCompanyRouter = Router();
 
 authCompanyRouter
   .post("/signup", signupCompanyController)
-  .post("/signin", signInCompanyController);
+  .post("/signin", signInCompanyController)
+  .get("/company/me", companyAuthMiddleware, getCompanyAuthMe);
 
 export default authCompanyRouter;
