c6f2e0c2dff56a86ddb50383c1828722f17d0b3b
24LP0893 24LP0893 <24LP0893@administrators-iMac-2.local>
2025-06-24 16:15:21 +0800

    Working day edit done

--- bookme/src/app/company/[companyName]/settings/_components/CompanyWorkingHours.tsx
+++ bookme/src/app/company/[companyName]/settings/_components/CompanyWorkingHours.tsx
@@ -1,83 +1,119 @@
 "use client";
+import { useCompanyAuth } from "@/app/_providers/CompanyAuthProvider";
+import { api } from "@/axios";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { WorkingHoursType } from "../../_components/CompanyTypes";
+import { LoadingSvg } from "@/app/_components/assets/LoadingSvg";
+
+const dayLabels: Record<keyof WorkingHoursType, string> = {
+  monday: "Даваа",
+  tuesday: "Мягмар",
+  wednesday: "Лхагва",
+  thursday: "Пүрэв",
+  friday: "Баасан",
+  saturday: "Бямба",
+  sunday: "Ням",
+};
 
 export const CompanyWorkingHours = () => {
-  const [openDays, setOpenDays] = useState({
-    openingHours: {
-      Даваа: { open: "09:00", close: "18:00", closed: false },
-      Мямар: { open: "09:00", close: "18:00", closed: false },
-      Лхагва: { open: "09:00", close: "18:00", closed: false },
-      Пүрэв: { open: "09:00", close: "18:00", closed: false },
-      Баасан: { open: "09:00", close: "18:00", closed: false },
-      Бямба: { open: "10:00", close: "18:00", closed: false },
-      Ням: { open: "10:00", close: "16:00", closed: false },
-    },
-  });
-
-  const days = Object.keys(openDays.openingHours) as Array<
-    keyof typeof openDays.openingHours
-  >;
+  const { company } = useCompanyAuth();
+  const [loading, setLoading] = useState(false);
+  if (!company) return <LoadingSvg />;
+  const [changedTimeSchedule, setChangedTimeSchedule] = useState(
+    company?.workingHours
+  );
 
   return (
-    <div className="bg-white w-full rounded-2xl p-4 flex flex-col gap-4">
+    <div className="w-full h-fit rounded-2xl p-3 bg-white">
       <div>
-        <div className="text-[20px] font-bold">Ажлын цаг засварлах</div>
+        <div className="text-[20px] font-bold">Ажлын цаг</div>
         <div className="text-[14px] font-normal text-[#aaa]">
-          Даваа - Ням гараг
+          Ажлын өдрийн төлөв өөрчлөх хэсэг.
         </div>
       </div>
-
-      {days.map((day, i) => {
-        return (
-          <div
-            className="text-[14px] font-semibold w-full flex justify-between items-center px-4"
-            key={i}
-          >
-            <div>{day}</div>
-            <div className="flex gap-3 items-center w-fit">
-              <Input
-                type="time"
-                value={openDays.openingHours[day].open}
-                onChange={(e) =>
-                  setOpenDays({
-                    ...openDays,
-                    openingHours: {
-                      ...openDays.openingHours,
+      <div className="flex flex-wrap gap-10">
+        {changedTimeSchedule &&
+          Object.entries(changedTimeSchedule)
+            .filter(([key]) => key !== "_id")
+            .map(([day, data]) => (
+              <div
+                key={day}
+                className="w-fit flex flex-col gap-3 items-center p-3 border-2 rounded-2xl bg-gray-50"
+              >
+                <div className="w-full flex justify-center ">
+                  <div className="w-fit bg-gray-300 rounded-3xl px-3">
+                    {dayLabels[day as keyof WorkingHoursType]}
+                  </div>
+                </div>
+                <div className="w-fit flex gap-3 ">
+                  <Input
+                    type="time"
+                    value={data.open}
+                    onChange={(e) =>
+                      setChangedTimeSchedule((prev) => ({
+                        ...prev!,
+                        [day]: {
+                          ...prev?.[day as keyof WorkingHoursType],
+                          open: e.target.value,
+                        },
+                      }))
+                    }
+                  />
+                  <Input
+                    type="time"
+                    value={data.close}
+                    onChange={(e) =>
+                      setChangedTimeSchedule((prev) => ({
+                        ...prev!,
+                        [day]: {
+                          ...prev?.[day as keyof WorkingHoursType],
+                          close: e.target.value,
+                        },
+                      }))
+                    }
+                  />
+                </div>
+                <Button
+                  variant="outline"
+                  onClick={async () => {
+                    const updatedClosed = !data.closed;
+                    setChangedTimeSchedule((prev) => ({
+                      ...prev!,
                       [day]: {
-                        ...openDays.openingHours[day],
-                        open: e.target.value,
+                        ...prev?.[day as keyof WorkingHoursType],
+                        closed: updatedClosed,
                       },
-                    },
-                  })
-                }
-                disabled={openDays.openingHours[day].closed}
-              />
-              <span>-</span>
-              <Input
-                type="time"
-                value={openDays.openingHours[day].close}
-                onChange={(e) =>
-                  setOpenDays({
-                    ...openDays,
-                    openingHours: {
-                      ...openDays.openingHours,
-                      [day]: {
-                        ...openDays.openingHours[day],
-                        close: e.target.value,
-                      },
-                    },
-                  })
-                }
-                disabled={openDays.openingHours[day].closed}
-              />
-            </div>
-          </div>
-        );
-      })}
-      <div className="w-full flex justify-end">
-        <Button className="w-fit">Update hours</Button>
+                    }));
+                    try {
+                      await api.put(`company/${company._id}`, {
+                        workingHours: {
+                          ...changedTimeSchedule,
+                          [day]: {
+                            ...changedTimeSchedule?.[
+                              day as keyof WorkingHoursType
+                            ],
+                            closed: updatedClosed,
+                          },
+                        },
+                      });
+                      toast.success(
+                        `${
+                          dayLabels[day as keyof WorkingHoursType]
+                        } амжилттай шинэчлэгдлээ`
+                      );
+                    } catch (error) {
+                      console.error("Хүсэлт илгээхэд алдаа:", error);
+                      toast.error("Сервертэй холбогдоход алдаа гарлаа.");
+                    }
+                  }}
+                >
+                  {data.closed ? "Нээх" : "Хаах"}
+                </Button>
+              </div>
+            ))}
       </div>
     </div>
   );
